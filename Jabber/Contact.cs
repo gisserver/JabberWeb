64136b297acfae3375811b932920485c616bee40
Cathal Ryan <cathalryan92@gmail.com>
2016-03-23 13:09:03 +0000

    New contact class added

---
+++ Jabber/Contact.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jabber
+{
+    public class Contact
+    {
+        private string name = "";
+        private string presence = "";
+        private string server = "";
+
+        public string JID_Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string JID_Presence
+        {
+            get
+            {
+                return this.presence;
+            }
+            set
+            {
+                this.presence = value;
+            }
+        }
+        public string JID_Server
+        {
+            get
+            {
+                return this.server;
+            }
+            set
+            {
+                this.server = value;
+            }
+        }
+
+
+    }
+}
--- Jabber/Main.aspx.cs
+++ Jabber/Main.aspx.cs
@@ -171,46 +171,5 @@ namespace Jabber
        
     }
 
-    public class Contact
-    {
-        private string name = "";
-        private string presence = "";
-        private string server = "";
-
-        public string JID_Name
-        {
-            get
-            {
-                return this.name;
-            }
-            set
-            {
-                this.name = value;
-            }
-        }
-        public string JID_Presence
-        {
-            get
-            {
-                return this.presence;
-            }
-            set
-            {
-                this.presence = value;
-            }
-        }
-        public string JID_Server
-        {
-            get
-            {
-                return this.server;
-            }
-            set
-            {
-                this.server = value;
-            }
-        }
-
-
-    }
+    
 }
